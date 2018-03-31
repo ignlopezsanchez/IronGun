e39b03d947acd2aacb20b8667eb2f035828995d8
Nacho Lopez <ignlopezsanchez@gmail.com>
2018-03-31 19:31:01 +0200

    rotate probe

--- code/js/player.js
+++ code/js/player.js
@@ -7,8 +7,14 @@ function Player(game){
 } 
 
 Player.prototype.draw = function(){
+  this.game.ctx.save();
   this.game.ctx.drawImage(this.img, this.x, this.y);
-
+  this.game.ctx.translate(this.game.canvas.width/2, this.game.canvas.height/2);
+  this.game.ctx.rotate((Math.PI/180)*45);
+  this.game.ctx.drawImage(this.img, this.x + 200, this.y);
+  this.game.ctx.restore();
+  this.game.ctx.drawImage(this.img, this.x + 200, this.y);
+;
 }
 
 
