9f973eeea334f0b637eeab98e689015ecdc2bcac
Nacho Lopez <ignlopezsanchez@gmail.com>
2018-04-03 15:22:16 +0200

    antes de meter controles simultaneos

--- js/bullet.js
+++ js/bullet.js
@@ -8,6 +8,7 @@ function Bullet(game, person) {
   this.h = 10;
   this.angle = person.angle;
   this.person = person;
+  this.r = 2;
 }
 
 Bullet.prototype.draw = function() {
@@ -15,30 +16,18 @@ Bullet.prototype.draw = function() {
   this.game.ctx.save(); 
   this.game.ctx.translate(this.x + this.person.img.width/2, this.y + this.person.img.height/2);
   this.game.ctx.rotate(angleRadians);
-  this.game.ctx.fillStyle = "red";
-  this.game.ctx.fillRect(0, 0, -this.w/2, -this.h/2);
-  this.game.ctx.restore();
-
-
-
-
-
-
-
-  // this.game.ctx.beginPath();
-  // this.game.ctx.fillStyle="green";
-  // this.game.ctx.strokeStyle="black";
-  // this.game.ctx.arc(this.x, this.y, this.r, 0, Math.PI * 2);
-  // this.game.ctx.fill();
-  // this.game.ctx.stroke();
-  // this.game.ctx.closePath();
+  this.game.ctx.beginPath();
+  this.game.ctx.fillStyle="black";
+  this.game.ctx.strokeStyle="black";
+  this.game.ctx.arc(0, 0, this.r, 0, Math.PI * 2);
+  this.game.ctx.fill();
+  this.game.ctx.stroke();
+  this.game.ctx.closePath();
+  this.game.ctx.restore();  
 }
 
 Bullet.prototype.move = function() {
   var angleRadians = (Math.PI/180)*(360-this.angle);
     this.x += Math.cos(angleRadians)*this.vx;
     this.y -= Math.sin(angleRadians)*this.vx;
-  
-  // this.x += this.vx; 
-  // this.y += this.vy;
 };
--- js/player.js
+++ js/player.js
@@ -45,6 +45,41 @@ Player.prototype.setListeners = function(){
     }
   }.bind(this)
 
+
+
+
+
+ document.onkeydown = function(event) {
+    var background = this.game.background;
+    switch (event.keyCode) {
+      case RIGHT_KEY:
+        this.rotateRight();
+        break;
+      case LEFT_KEY:
+      this.rotateLeft();        
+        break;
+      case UP_KEY:
+          
+          this.moveForward();
+          
+        break;
+
+      case DOWN_KEY:        
+       
+          this.moveBackward();
+        
+      break;
+        
+        break;
+      case SPACEBAR_KEY:
+        this.shoot();
+        
+        break;    
+      
+    }
+  }.bind(this)
+
+
 };
 
 
